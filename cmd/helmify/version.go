45ef82eaf914e7a56b9315b067ab9a45ad8044a1
Artem Torubarov <torubarov-a-a@yandex.ru>
2021-09-19 22:15:13 +0300

    go 1.17; move to goreleaser

--- cmd/helmify/version.go
+++ cmd/helmify/version.go
@@ -4,17 +4,15 @@ import (
 	"fmt"
 )
 
-// these information will be collected when build, by `-ldflags "-X main.appVersion=0.1"`.
+// these information will be collected when build, by `-ldflags "-X main.version=0.1"`.
 var (
-	appVersion = "development"
-	buildTime  = "not set"
-	gitCommit  = "not set"
-	gitRef     = "not set"
+	version = "development"
+	date    = "not set"
+	commit  = "not set"
 )
 
 func printVersion() {
-	fmt.Printf("Version:    %s\n", appVersion)
-	fmt.Printf("Build Time: %s\n", buildTime)
-	fmt.Printf("Git Commit: %s\n", gitCommit)
-	fmt.Printf("Git Ref:    %s\n", gitRef)
+	fmt.Printf("Version:    %s\n", version)
+	fmt.Printf("Build Time: %s\n", date)
+	fmt.Printf("Git Commit: %s\n", commit)
 }
