2179803cdc01ff4045d01cfdd993b2fba0fbc414
dongge.tan <dontan001@hotmail.com>
2022-01-21 10:50:43 +0800

    add blank space between named temlates

--- pkg/helm/init.go
+++ pkg/helm/init.go
@@ -43,6 +43,7 @@ Expand the name of the chart.
 {{- define "<CHARTNAME>.name" -}}
 {{- default .Chart.Name .Values.nameOverride | trunc 63 | trimSuffix "-" }}
 {{- end }}
+
 {{/*
 Create a default fully qualified app name.
 We truncate at 63 chars because some Kubernetes name fields are limited to this (by the DNS naming spec).
@@ -60,12 +61,14 @@ If release name contains chart name it will be used as a full name.
 {{- end }}
 {{- end }}
 {{- end }}
+
 {{/*
 Create chart name and version as used by the chart label.
 */}}
 {{- define "<CHARTNAME>.chart" -}}
 {{- printf "%s-%s" .Chart.Name .Chart.Version | replace "+" "_" | trunc 63 | trimSuffix "-" }}
 {{- end }}
+
 {{/*
 Common labels
 */}}
@@ -77,6 +80,7 @@ app.kubernetes.io/version: {{ .Chart.AppVersion | quote }}
 {{- end }}
 app.kubernetes.io/managed-by: {{ .Release.Service }}
 {{- end }}
+
 {{/*
 Selector labels
 */}}
@@ -84,6 +88,7 @@ Selector labels
 app.kubernetes.io/name: {{ include "<CHARTNAME>.name" . }}
 app.kubernetes.io/instance: {{ .Release.Name }}
 {{- end }}
+
 {{/*
 Create the name of the service account to use
 */}}
