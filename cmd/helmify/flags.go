3859ee93e4c0f9a3a4494f273b9532b523b7ee22
gprossliner <6724584+gprossliner@users.noreply.github.com>
2022-12-10 21:49:10 +0100

    flag help

--- cmd/helmify/flags.go
+++ cmd/helmify/flags.go
@@ -37,7 +37,7 @@ func ReadFlags() config.Config {
 	flag.BoolVar(&result.Verbose, "v", false, "Enable verbose output (print WARN & INFO). Example: helmify -v")
 	flag.BoolVar(&result.VeryVerbose, "vv", false, "Enable very verbose output. Same as verbose but with DEBUG. Example: helmify -vv")
 	flag.BoolVar(&crd, "crd-dir", false, "Enable crd install into 'crds' directory.\nWarning: CRDs placed in 'crds' directory will not be templated by Helm.\nSee https://helm.sh/docs/chart_best_practices/custom_resource_definitions/#some-caveats-and-explanations\nExample: helmify -crd-dir")
-	flag.BoolVar(&imagePullSecrets.Enabled, "image-pull-secrets", false, "Enables imagePullSecrets in values.yaml to specify existing secrets.")
+	flag.BoolVar(&imagePullSecrets.Enabled, "image-pull-secrets", false, "Allows the user to use existing secrets as imagePullSecrets in values.yaml")
 
 	flag.Parse()
 	if h || help {
