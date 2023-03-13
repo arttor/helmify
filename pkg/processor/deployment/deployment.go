3f37f696fa92783e74ee2b564d24e169c13dc523
RealAnna <anna.reale@dynatrace.com>
2023-03-13 09:47:49 +0100

    hide autofill behind CLI flag
    Signed-off-by: RealAnna <anna.reale@dynatrace.com>

--- pkg/processor/deployment/deployment.go
+++ pkg/processor/deployment/deployment.go
@@ -165,7 +165,7 @@ func (d deployment) Process(appMeta helmify.AppMetadata, obj *unstructured.Unstr
 		imagePullSecrets.ProcessSpecMap(specMap, &values)
 	}
 
-	spec := constraints.ProcessSpecMap(nameCamel, specMap, &values)
+	spec := constraints.ProcessSpecMap(nameCamel, specMap, &values, appMeta.Config().GenerateDefault)
 	spec = strings.ReplaceAll(spec, "'", "")
 
 	return true, &result{
