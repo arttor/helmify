68c4c7959a26ac901f4ac534bfc2ebedcecd9a9c
hfuss <hayden.fuss@kaleido.io>
2023-04-21 14:11:52 -0400

    [serviceaccount] Annotations as Values Support
    Signed-off-by: hfuss <hayden.fuss@kaleido.io>

--- pkg/processor/rbac/serviceaccount.go
+++ pkg/processor/rbac/serviceaccount.go
@@ -1,12 +1,14 @@
 package rbac
 
 import (
-	"io"
-
+	"fmt"
 	"github.com/arttor/helmify/pkg/helmify"
-	"github.com/arttor/helmify/pkg/processor"
+	yamlformat "github.com/arttor/helmify/pkg/yaml"
+	"github.com/iancoleman/strcase"
+	"io"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime/schema"
+	"strings"
 )
 
 var serviceAccountGVC = schema.GroupVersionKind{
@@ -27,25 +29,78 @@ func (sa serviceAccount) Process(appMeta helmify.AppMetadata, obj *unstructured.
 	if obj.GroupVersionKind() != serviceAccountGVC {
 		return false, nil, nil
 	}
-	meta, err := processor.ProcessObjMeta(appMeta, obj)
+	meta, values, err := sa.processServiceAccountMeta(appMeta, obj)
 	if err != nil {
 		return true, nil, err
 	}
+
 	return true, &saResult{
-		data: []byte(meta),
+		data:   []byte(meta),
+		values: values,
 	}, nil
 }
 
+const metaTeml = `apiVersion: %[1]s
+kind: %[2]s
+metadata:
+  name: %[3]s
+  labels:
+%[5]s
+  {{- include "%[4]s.labels" . | nindent 4 }}
+%[6]s
+  {{ toYaml .Values.%[7]s.annotations | nindent 4 }}`
+
+func (sa serviceAccount) processServiceAccountMeta(appMeta helmify.AppMetadata, obj *unstructured.Unstructured) (string, helmify.Values, error) {
+	var err error
+	var labels, annotations string
+	values := helmify.Values{}
+	name := strcase.ToLowerCamel(appMeta.TrimName(obj.GetName()))
+	err = unstructured.SetNestedField(values, map[string]interface{}{}, name, "annotations")
+	if err != nil {
+		return "", nil, err
+	}
+	if len(obj.GetLabels()) != 0 {
+		l := obj.GetLabels()
+		// provided by Helm
+		delete(l, "app.kubernetes.io/name")
+		delete(l, "app.kubernetes.io/instance")
+		delete(l, "app.kubernetes.io/version")
+		delete(l, "app.kubernetes.io/managed-by")
+		delete(l, "helm.sh/chart")
+
+		// Since we delete labels above, it is possible that at this point there are no more labels.
+		if len(l) > 0 {
+			labels, err = yamlformat.Marshal(l, 4)
+			if err != nil {
+				return "", nil, err
+			}
+		}
+	}
+	if len(obj.GetAnnotations()) != 0 {
+		annotations, err = yamlformat.Marshal(map[string]interface{}{"annotations": obj.GetAnnotations()}, 2)
+		if err != nil {
+			return "", nil, err
+		}
+	}
+	templatedName := appMeta.TemplatedName(obj.GetName())
+	apiVersion, kind := obj.GetObjectKind().GroupVersionKind().ToAPIVersionAndKind()
+	metaStr := fmt.Sprintf(metaTeml, apiVersion, kind, templatedName, appMeta.ChartName(), labels, annotations, name)
+	metaStr = strings.Trim(metaStr, " \n")
+	metaStr = strings.ReplaceAll(metaStr, "\n\n", "\n")
+	return metaStr, values, nil
+}
+
 type saResult struct {
-	data []byte
+	data   []byte
+	values helmify.Values
 }
 
 func (r *saResult) Filename() string {
-	return "deployment.yaml"
+	return "serviceaccount.yaml"
 }
 
 func (r *saResult) Values() helmify.Values {
-	return helmify.Values{}
+	return r.values
 }
 
 func (r *saResult) Write(writer io.Writer) error {
